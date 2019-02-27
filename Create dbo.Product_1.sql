e3b0c4ddd3d1aee5166c9fc6b0896e6975c8e7dd
sriragt2 <46505933+sriragt2@users.noreply.github.com>
2019-02-26 21:27:17 -0500

    Add files via upload

---
+++ Create dbo.Product_1.sql
@@ -0,0 +1,20 @@
+﻿USE [Finance]
+GO
+
+/****** Object: Table [dbo].[Product] Script Date: 2/26/2019 9:24:02 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Product] (
+    [Product Id]  INT           NOT NULL,
+    [P_Name]      VARCHAR (50)  NULL,
+    [P_Price]     FLOAT (53)    NULL,
+    [P_Category]  NVARCHAR (50) NULL,
+    [P_Discount%] FLOAT (53)    NULL,
+    PRIMARY KEY CLUSTERED ([Product Id] ASC)
+);
+
+
---
+++ Create dbo.Sales.sql
@@ -0,0 +1,20 @@
+﻿USE [Finance]
+GO
+
+/****** Object: Table [dbo].[Sales] Script Date: 2/26/2019 9:25:20 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Sales] (
+    [SaleId]      INT      NOT NULL,
+    [Product Id]  INT      NOT NULL,
+    [Sale_Date]   DATETIME NULL,
+    [Sales_Count] INT      NULL,
+    PRIMARY KEY CLUSTERED ([SaleId] ASC),
+    CONSTRAINT [FK_Sales_Toproduct] FOREIGN KEY ([Product Id]) REFERENCES [dbo].[Product] ([Product Id])
+);
+
+
